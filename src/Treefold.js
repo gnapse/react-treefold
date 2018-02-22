7f0da51fc02b88afa831fb539db2a1bf58e60743
Ernesto Garcia <gnapse@gmail.com>
2018-02-22 11:20:03 -0300

    Convert Treefold to class component form

--- src/Treefold.js
+++ src/Treefold.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Node from './Node';
 import ToggleController from './ToggleController';
 
-const Treefold = ({
-  nodes,
-  isNodeExpanded,
-  onToggleExpand,
-  getNodeId,
-  getNodeChildren,
-  renderNode,
-  renderEmptyFolder,
-}) => {
-  const isControlled =
-    typeof isNodeExpanded !== 'undefined' &&
-    typeof onToggleExpand !== 'undefined';
-  const render = toggle =>
-    nodes.map(node =>
+class Treefold extends Component {
+  static propTypes = {
+    nodes: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+    isNodeExpanded: PropTypes.func,
+    onToggleExpand: PropTypes.func,
+    getNodeId: PropTypes.func,
+    getNodeChildren: PropTypes.func,
+    renderNode: PropTypes.func.isRequired,
+    renderEmptyFolder: PropTypes.func,
+  };
+
+  static defaultProps = {
+    getNodeId: item => item.id,
+    getNodeChildren: item => item.children,
+    renderEmptyFolder: () => null,
+  };
+
+  internalRender = ({ isOn, onToggle }) => {
+    const {
+      nodes,
+      getNodeId,
+      getNodeChildren,
+      renderNode,
+      renderEmptyFolder,
+    } = this.props;
+    return nodes.map(node =>
       React.createElement(Node, {
         key: getNodeId(node),
         node,
@@ -24,31 +36,25 @@ const Treefold = ({
         renderEmptyFolder,
         getNodeId,
         getNodeChildren,
-        isNodeExpanded: toggle.isOn,
-        onToggleExpand: toggle.onToggle,
+        isNodeExpanded: isOn,
+        onToggleExpand: onToggle,
       })
     );
-  return isControlled ? (
-    render({ isOn: isNodeExpanded, onToggle: onToggleExpand })
-  ) : (
-    <ToggleController getId={getNodeId}>{render}</ToggleController>
-  );
-};
+  };
 
-Treefold.propTypes = {
-  nodes: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
-  isNodeExpanded: PropTypes.func,
-  onToggleExpand: PropTypes.func,
-  getNodeId: PropTypes.func,
-  getNodeChildren: PropTypes.func,
-  renderNode: PropTypes.func.isRequired,
-  renderEmptyFolder: PropTypes.func,
-};
-
-Treefold.defaultProps = {
-  getNodeId: item => item.id,
-  getNodeChildren: item => item.children,
-  renderEmptyFolder: () => null,
-};
+  render() {
+    const { isNodeExpanded, onToggleExpand, getNodeId } = this.props;
+    const isControlled =
+      typeof isNodeExpanded === 'function' &&
+      typeof onToggleExpand === 'function';
+    return isControlled ? (
+      this.internalRender({ isOn: isNodeExpanded, onToggle: onToggleExpand })
+    ) : (
+      <ToggleController getId={getNodeId}>
+        {this.internalRender}
+      </ToggleController>
+    );
+  }
+}
 
 export default Treefold;
